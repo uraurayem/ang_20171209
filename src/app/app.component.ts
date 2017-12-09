1ac8615d369c0e98f7928c17bfc1ea5c5d5d70e0
finalwoo <finalu763@gmail.com>
2017-12-09 16:12:52 +0900

    20171209

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {User } from './user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  userList:Array<User> = []; 
+  
+  userName:string = "" ;
+  userAge:number = 0;
+
+  constructor(){
+   
+  }
+  addUser():void {
+  
+    var user :User = new User() ; 
+    user.userName = this.userName ;
+    user.userAge = this.userAge;
+
+    this.userList.push(user);
+  }
+  test() : void {
+
+    var user1:User = new User();
+    user1.userName = '미선';
+    user1.userAge = 5;
+    this.userList.push(user1);
+
+    user1 = new User();
+    user1.userName = '미선2';
+    user1.userAge = 6;
+    this.userList.push(user1);
+
+  }
+
 }
+
---
+++ src/app/user.ts
@@ -0,0 +1,5 @@
+// export 롤 선언하면 import 로 사용가능
+export class User {
+    userName : string ; 
+    userAge : number ; 
+}
